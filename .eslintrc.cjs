1f01d3edeca5a204c9e394b015f85f7aea35b017
i5x64BIT <vaintomer@gmail.com>
2023-09-24 23:46:17 +0300

    Remove parser

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,6 @@ module.exports = {
     "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@babel/eslint-parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
